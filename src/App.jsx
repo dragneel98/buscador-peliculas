9c1756dbee55cb547bdae785bbb7d4a954e02e0e
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-11-15 12:53:21 -0300

    fix checkout and movies message

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback} from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import './App.css'
 import Movies from './components/movies'
 import { useMovies } from './hooks/useMovies'
@@ -9,52 +9,52 @@ function useSearch() {
   const [search, setSearch] = useState("")
   const [error, setError] = useState(null)
   const isFirstInput = useRef(true)
-    // alternativa controlada por react usando useState (es mas lenta pero es mas facil hacer validaciones)
-    // al tener un estado asociado al input lo podemos controlar en tiempo real
-    // se suele recomendar la no controlada por que es mas facil, rapida y optima 
+  // alternativa controlada por react usando useState (es mas lenta pero es mas facil hacer validaciones)
+  // al tener un estado asociado al input lo podemos controlar en tiempo real
+  // se suele recomendar la no controlada por que es mas facil, rapida y optima 
 
-    useEffect(() => {
-      if(isFirstInput.current){
-        isFirstInput.current = search === ""
-        return
-      }
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ""
+      return
+    }
 
-      if(search === "") {
-        setError("no se puede dejar vacio")
-        return
-      }
-      setError(null)
-    }, [search])
+    if (search === "") {
+      setError("no se puede dejar vacio")
+      return
+    }
+    setError(null)
+  }, [search])
 
 
-    // alternativa sin useRef
-   
-    // alternativa para recuperar multiples inputs 
-    // const inputs = Object.fromEntries(new FormData(event.target))
-    // console.log(inputs);
+  // alternativa sin useRef
 
-    // const inputs = new FormData(event.target)
-    // const search = inputs.get("search")
-    // console.log(search);
-  
-   // alternativa con useRef
-   // const inputRef = useRef()
-   // const handleSubmit = (event) =>{
-    // siempre usar current para acceder al valor de un ref
-    // const inputValue = inputRef.current.value 
-    // console.log(inputsValue);
+  // alternativa para recuperar multiples inputs 
+  // const inputs = Object.fromEntries(new FormData(event.target))
+  // console.log(inputs);
 
-    return {search, setSearch, error}
+  // const inputs = new FormData(event.target)
+  // const search = inputs.get("search")
+  // console.log(search);
+
+  // alternativa con useRef
+  // const inputRef = useRef()
+  // const handleSubmit = (event) =>{
+  // siempre usar current para acceder al valor de un ref
+  // const inputValue = inputRef.current.value 
+  // console.log(inputsValue);
+
+  return { search, setSearch, error }
 }
 
 function App() {
   const [sort, setSort] = useState(false)
-  const {search, setSearch, error} = useSearch()
-  const {movies, getMovies, loading} = useMovies({search, sort})
+  const { search, setSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies({search})
+    getMovies({ search })
   }
 
   const debouncedGetMovies = useCallback(
@@ -65,10 +65,10 @@ function App() {
     , []
   )
 
-  
 
-  const handleChange = (event) =>{
-    const newSearch = event.target.value 
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
     setSearch(newSearch)
     debouncedGetMovies(newSearch)
 
@@ -80,17 +80,21 @@ function App() {
     <>
       <header>
         <h1> buscador de peliculas</h1>
-        <form className='form' 
-        onSubmit={handleSubmit}
+        <form className='form'
+          onSubmit={handleSubmit}
         >
-        <input name='search' onChange={handleChange} value={search} placeholder='Avengers, Star Wars, etc'></input>
-        <input type="checkbox" onChange={handleSort} checked={sort}></input>
-        <button  type='submit'>buscar</button>
+          <input className='search' name='search' onChange={handleChange} value={search} placeholder='Avengers, Star Wars, etc'></input>
+          <div className='form-div'>
+            <label>ordenar de alfabeticamente</label>
+            <input className='checkbox' type="checkbox" onChange={handleSort} checked={sort}></input>
+            <button type='submit'>buscar</button>
+          </div>
+
         </form>
         {error && <p> {error} </p>}
       </header>
       <main className='main'>
-        {loading ? <p>cargando...</p> : <Movies movies={movies}/>}
+        {loading ? <p>cargando...</p> : <Movies movies={movies} />}
       </main>
     </>
   )
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
 import "./style/movies.css"
 
- function RenderMovies({movies}) {
+function RenderMovies({ movies }) {
   return (
     <>
-        <ul className='movies'>
-            {movies.map(movie => (
-              <li key={movie.id}>
-                <h3> {movie.title} </h3>
-                <img src={movie.poster} alt={movie.title}></img>
-              </li>
-            ))}
-          </ul>
+      <ul className='movies'>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <h3> {movie.title} </h3>
+            <img src={movie.poster} alt={movie.title}></img>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
 
- function NoMovies() {
-    return (
-      <>
-          <h3> nose encontraron resultados que coincidan con tu busqueda </h3>
-      </>
-    )
-  }
-  
-export default function Movies({movies}) {
- 
-    const hasMovies = movies?.length > 0
-    return(
-     hasMovies 
-        ? <RenderMovies movies={movies} /> 
-        : <NoMovies/>
-        )
+function NoMovies({ movies }) {
+
+  return (
+    <>
+      {movies != ""
+        ? <h3> nose encontraron resultados que coincidan con tu busqueda </h3>
+        : "ingrese una pelicula"
+      }
+
+    </>
+  )
+}
+
+export default function Movies({ movies }) {
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies
+      ? <RenderMovies movies={movies} />
+      : <NoMovies movies={movies} />
+  )
 }
